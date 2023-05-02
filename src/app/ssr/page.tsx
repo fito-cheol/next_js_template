bfe85dae74db191b9a87c7c3335fbdca12eb7c10
김철 <dmsghs92@gmail.com>
2023-05-02 16:44:14 +0900

    Add docker-compose

--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -8,12 +8,13 @@ interface BookInfo {
 }
 
 export default async function ServerAndClient() {
-  const data = await getJson();
+  // const data = await getJson();
+  const data = { title: 'Book Title', description: 'Book Description' };
   return (
-    <div className='flex h-full w-full flex-col items-center justify-center bg-gray-100 '>
-      <h1 className='text-3xl font-bold'>Server and Client Components</h1>
+    <div className="flex h-full w-full flex-col items-center justify-center bg-gray-100 ">
+      <h1 className="text-3xl font-bold">Server and Client Components</h1>
       <Server bookInfo={data} />
-      <div className='flex w-full max-w-3xl flex-col space-y-8 rounded-lg bg-white p-4 shadow'></div>
+      <div className="flex w-full max-w-3xl flex-col space-y-8 rounded-lg bg-white p-4 shadow"></div>
     </div>
   );
 }
